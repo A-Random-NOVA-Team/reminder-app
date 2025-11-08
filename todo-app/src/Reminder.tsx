230a181d346deec01f7996bcfaa75ab75698826c
Ankita Varigonda <avarigonda18@gmail.com>
2025-11-08 15:29:12 -0500

    add reminder

--- todo-app/src/Reminder.tsx
+++ todo-app/src/Reminder.tsx
@@ -1,10 +1,14 @@
-function Reminder() {
+function Reminder(props: {text:string, key:number}) {
+    
+    const handleClick = () => {
+        console.log('Button clicked!');
+     };
+
     return (
         <>
         <div className="row">
-            <div className="col-sm-2"><input type="checkbox"/></div>
-            <div className="col-sm-10" style={{ textAlign: 'left' }}>Reminder Text</div>
-            
+            <div className="col-sm-2" onClick={handleClick}><input type="checkbox"/></div>
+            <div className="col-sm-10" style={{ textAlign: 'left' }}>{ props.text }</div>        
         </div>
         </>
     );
